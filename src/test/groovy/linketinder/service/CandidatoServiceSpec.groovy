65cefa0a79cb75a661c67dbfd3428829d0007a18
Henrique Roberto dos Santos <contatohenrique.hrs@gmail.com>
2026-04-26 12:55:44 -0300

    test(service): reescreve specs com Mock(IDao) via injeção de dependência

--- src/test/groovy/linketinder/service/CandidatoServiceSpec.groovy
+++ src/test/groovy/linketinder/service/CandidatoServiceSpec.groovy
@@ -2,24 +2,46 @@ package linketinder.service
 
 import spock.lang.Specification
 import linketinder.model.Candidato
-import linketinder.dao.CandidatoDAO
+import linketinder.dao.ICandidatoDAO
 
 class CandidatoServiceSpec extends Specification {
 
+    ICandidatoDAO mockDAO
+    CandidatoService service
+
+    def setup() {
+        mockDAO = Mock(ICandidatoDAO)
+        service = new CandidatoService(mockDAO)
+    }
+
     def "Deve lançar exceção ao cadastrar candidato com e-mail duplicado"() {
-        given: "Um CandidatoDAO mockado que simula e-mail já existente"
-        def candidatoExistente = new Candidato("Ana Silva", "ana@email.com", "11111111111", 22, "SC", "88000-000", "Dev", [], "senha123")
-        def mockDAO = Mock(CandidatoDAO)
-        mockDAO.listar() >> [candidatoExistente]
+        given: "O DAO informa que o e-mail já existe"
+        mockDAO.existeEmail("ana@email.com") >> true
 
-        and: "Um novo candidato com o mesmo e-mail"
-        def novoCandidato = new Candidato("Carlos", "ana@email.com", "22222222222", 30, "SP", "01000-000", "Dev", [], "outrasenha")
+        and: "Um candidato com e-mail duplicado"
+        def candidato = new Candidato("Carlos", "ana@email.com", "22222222222", 30, "SP", "01000-000", "Dev", [], "senha")
 
-        when: "Tentamos cadastrar o candidato duplicado verificando o e-mail"
-        boolean emailJaCadastrado = [candidatoExistente].any { it.email.equalsIgnoreCase(novoCandidato.email) }
+        when: "Tentamos cadastrar"
+        service.cadastrar(candidato)
 
-        then: "O e-mail deve ser detectado como duplicado"
-        emailJaCadastrado == true
+        then: "Deve lançar IllegalArgumentException"
+        thrown(IllegalArgumentException)
+    }
+
+    def "Deve cadastrar candidato com e-mail novo"() {
+        given: "O DAO informa que o e-mail não existe"
+        mockDAO.existeEmail("novo@email.com") >> false
+        mockDAO.inserir(_) >> 1
+
+        and: "Um candidato novo"
+        def candidato = new Candidato("Ana", "novo@email.com", "11111111111", 22, "SC", "88000-000", "Dev", [], "senha")
+
+        when: "Cadastramos"
+        service.cadastrar(candidato)
+
+        then: "O id deve ser preenchido"
+        candidato.id == 1
+        1 * mockDAO.inserir(candidato)
     }
 
     def "Deve criar candidato com os dados fornecidos corretamente"() {
--- src/test/groovy/linketinder/service/LoginServiceSpec.groovy
+++ src/test/groovy/linketinder/service/LoginServiceSpec.groovy
@@ -3,29 +3,45 @@ package linketinder.service
 import spock.lang.Specification
 import linketinder.model.Candidato
 import linketinder.model.Empresa
+import linketinder.dao.ICandidatoDAO
+import linketinder.dao.IEmpresaDAO
+import linketinder.dao.IVagaDAO
+import linketinder.dao.ICompetenciaDAO
 
 class LoginServiceSpec extends Specification {
 
+    CandidatoService candidatoService
+    EmpresaService empresaService
+    LoginService loginService
+
+    def setup() {
+        def mockCandidatoDAO   = Mock(ICandidatoDAO)
+        def mockEmpresaDAO     = Mock(IEmpresaDAO)
+        def mockVagaDAO        = Mock(IVagaDAO)
+        def mockCompetenciaDAO = Mock(ICompetenciaDAO)
+
+        def candidato = new Candidato("Ana Silva", "ana@email.com", "11111111111", 22, "SC", "88000-000", "Dev", [], "senha123")
+        def empresa   = new Empresa("TechSul", "rh@techsul.com", "11111111000101", "Brasil", "RS", "90000-100", "Desc", "corp123")
+
+        mockCandidatoDAO.listar() >> [candidato]
+        mockEmpresaDAO.listar()   >> [empresa]
+
+        candidatoService = new CandidatoService(mockCandidatoDAO)
+        empresaService   = new EmpresaService(mockEmpresaDAO, mockVagaDAO, mockCompetenciaDAO)
+        loginService     = new LoginService(candidatoService, empresaService)
+    }
+
     def "Deve retornar null ao tentar login com credenciais inválidas"() {
-        given: "Uma lista vazia de candidatos e empresas"
-        def candidatos = []
-        def empresas = []
+        when: "Login com credenciais que não existem"
+        def resultado = loginService.realizarLogin("inexistente@email.com", "errada")
 
-        when: "Buscamos por e-mail e senha que não existem"
-        def resultado = candidatos.find { it.email == "inexistente@email.com" && it.senha == "errada" }
-                ?: empresas.find { it.email == "inexistente@email.com" && it.senha == "errada" }
-
-        then: "O resultado deve ser null"
+        then: "Deve retornar null"
         resultado == null
     }
 
     def "Deve encontrar candidato com e-mail e senha corretos"() {
-        given: "Um candidato cadastrado"
-        def candidato = new Candidato("Ana Silva", "ana@email.com", "11111111111", 22, "SC", "88000-000", "Desenvolvedora", [], "senha123")
-        def candidatos = [candidato]
-
-        when: "O login é realizado com credenciais corretas"
-        def resultado = candidatos.find { it.email == "ana@email.com" && it.senha == "senha123" }
+        when: "Login com credenciais do candidato"
+        def resultado = loginService.realizarLogin("ana@email.com", "senha123")
 
         then: "Deve retornar o candidato"
         resultado instanceof Candidato
@@ -33,12 +49,8 @@ class LoginServiceSpec extends Specification {
     }
 
     def "Deve encontrar empresa com e-mail e senha corretos"() {
-        given: "Uma empresa cadastrada"
-        def empresa = new Empresa("TechSul", "rh@techsul.com", "11111111000101", "Brasil", "RS", "90000-100", "Desc", "corp123")
-        def empresas = [empresa]
-
-        when: "O login é realizado com credenciais da empresa"
-        def resultado = empresas.find { it.email == "rh@techsul.com" && it.senha == "corp123" }
+        when: "Login com credenciais da empresa"
+        def resultado = loginService.realizarLogin("rh@techsul.com", "corp123")
 
         then: "Deve retornar a empresa"
         resultado instanceof Empresa
